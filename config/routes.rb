6ac568080fef12bd67a26a0383035067524e950b
Thomas Cullen <cullentj@yahoo.com>
2011-12-28 12:24:18 -0600

    added users controller and layout links test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
   
 
   
