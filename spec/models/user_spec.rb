8aa985200bfdf1c893845d983ff241138e7232f2
Thomas Cullen <cullentj@yahoo.com>
2011-12-29 14:06:06 -0600

    added -password machinery

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -100,12 +100,56 @@ end
   
   describe "password encryption" do
   
-    before(:each) do
-      @user = User.create!(@attr)
-    end
+      before(:each) do
+        @user = User.create!(@attr)
+      end
   
-    it "should have an encrypted password attribute" do
-      @user.should respond_to(:encrypted_password)
+        it "should have an encrypted password attribute" do
+          @user.should respond_to(:encrypted_password)
+        end
+    
+        it "should set the encrypted password attribute" do
+          @user.encrypted_password.should_not be_blank
+        end
+    
+        it "should have a salt" do
+          @user.should respond_to(:salt)
+        end    
+    
+    describe "has_password? method" do
+    
+        it "should exist" do
+          @user.should respond_to(:has_password?)
+        end
+      
+        it "should return true if the passwords match" do
+          @user.has_password?(@attr[:password]).should be_true
+        end
+      
+        it "should return false if the passwords match" do
+          @user.has_password?("invalid").should be_false        
+        end
+    end
+    
+    
+    
+    describe "authenticate method" do
+      
+      it "should exist" do
+        User.should respond_to(:authenticate)
+      end
+        
+      it "should return nil on email/password mismatch" do
+        User.authenticate(@attr[:email], "wrongpass").should be_nil
+      end
+      
+      it "should return nil for an email address w no user" do
+        User.authenticate("bar@foo.com", @attr[:password]).should be_nil
+      end
+      
+      it "should return the user on email/password match" do
+        User.authenticate(@attr[:email], @attr[:password]).should == @user
+      end
     end
   end
 end
