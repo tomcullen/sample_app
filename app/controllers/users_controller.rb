58918b80e51f839f2dda777f1933413f13c55cba
Thomas Cullen <cullentj@yahoo.com>
2011-12-29 16:05:01 -0600

    added signin form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
-  def new
-    @title = "Sign up"
-  end
   
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+
+  def new
+    @title = "Sign up"
+    @user = User.new
   end
 
 end
