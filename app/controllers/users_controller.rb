3cea4ae683e27fec5d9f1bc9ba0a3c650ef56f8a
Thomas Cullen <cullentj@yahoo.com>
2012-01-03 10:28:52 -0600

    editing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,4 +25,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = "Edit user"
   end
+  
+  def update
+    @user = User.find(params[:id])    
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :flash => { :success => "Profile updated"}
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -71,7 +71,11 @@ describe "LayoutLinks" do
     it "should have a profile link" do
       visit root_path
       response.should have_selector("a", :href => user_path(@user), :content => "Profile")
-      
+    end
+    
+    it "should have a settings link" do
+      visit root_path
+      response.should have_selector("a", :href => edit_user_path(@user), :content => "Settings")
     end
   end
 end
