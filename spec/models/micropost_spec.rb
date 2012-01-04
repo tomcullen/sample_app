f7b2ee79f9db74aa1c1a8fe82140274fb427be6f
Thomas Cullen <cullentj@yahoo.com>
2012-01-04 09:32:30 -0600

    micropost validations

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -26,11 +26,20 @@ describe Micropost do
       @micropost.user.should == @user
     end
   end
-
-
-
-
-
-
+  
+  describe "validations" do
+    
+    it "should have a user id" do
+      Micropost.new(@attr).should_not be_valid
+    end
+  
+    it "should require nonblank content" do
+      @user.microposts.build(content: "   ").should_not be_valid
+    end
+    
+    it "should reject a long content" do
+      @user.microposts.build(content: "a" * 141).should_not be_valid
+    end
+  end
 end
 
