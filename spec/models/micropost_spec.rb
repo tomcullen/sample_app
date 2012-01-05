b7ba6cbed83c4effd5e4f9c97ee0fe5eb752a845
Thomas Cullen <cullentj@yahoo.com>
2012-01-05 16:56:38 -0600

    made a working feed

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -27,7 +27,7 @@ describe Micropost do
     end
   end
   
-  describe "validations" do
+  describe "validations" do 
     
     it "should have a user id" do
       Micropost.new(@attr).should_not be_valid
@@ -39,6 +39,39 @@ describe Micropost do
     
     it "should reject a long content" do
       @user.microposts.build(content: "a" * 141).should_not be_valid
+    end
+  end
+  
+  describe "from_users_followed_by" do
+    
+    before(:each) do
+      @other_user = Factory(:user, email: Factory.next(:email))
+      @third_user = Factory(:user, email: Factory.next(:email))
+      
+      @user_post = @user.microposts.create!(content: "foo")
+      @other_post = @other_user.microposts.create!(content: "bar")
+      @third_post = @third_user.microposts.create!(content: "baz")
+     
+     @user.follow!(@other_user)
+    end
+    
+    it "should have a from_users_followed_by method" do
+      Micropost.should respond_to(:from_users_followed_by)
+    end
+    
+    it "should include the followed users microposts" do
+      Micropost.from_users_followed_by(@user).should include(@other_post)
+      
+    end
+    
+    it "should include the users own microposts" do
+      Micropost.from_users_followed_by(@user).should include(@user_post)
+      
+    end
+    
+    it "should not include an unfollowed users microposts" do
+      Micropost.from_users_followed_by(@user).should_not include(@third_post)
+      
     end
   end
 end
